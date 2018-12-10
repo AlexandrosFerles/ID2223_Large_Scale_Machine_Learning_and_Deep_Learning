2f52981dbb8658ce2710400d5271895d6e2963be
ferles <ferles@kth.se>
2018-12-10 11:32:53 +0100

    initialised init folder so I can have the code outside the notebook

---
+++ Lab2_ConvNets_with_Tensorflow/src/Feed_Forward.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+from __future__ import division, print_function, unicode_literals
+
+
+if __name__=='__main__':
+    print('Finished!')
